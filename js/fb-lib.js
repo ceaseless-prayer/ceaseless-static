278ff49dfd0bc6b4f5f0d7e935cb3d53cdb57797
Chris <kirisu@gmail.com>
2014-10-30 23:19:51 -0700

    getting app code in

---
+++ js/fb-lib.js
@@ -0,0 +1,75 @@
+var fbAppId = '114260328766572';
+
+var refreshToken = function () {
+  $.ajax({
+    type: 'GET',
+    url: '/refreshToken.php',
+    dataType: 'json',
+    success: function (data) {
+      if (data.success) {
+        console.log("token refresh succeeded");
+      } else {
+        console.log("token refresh failed");
+      }
+    },
+    data: {
+      'access_token': FB.getAccessToken(),
+      'fb_id': FB.getUserID()
+    },
+    async: true
+  });
+};
+
+var authenticate = function (response) {
+  if(fbInitActions) {
+    if (response.status === 'connected') {
+      fbInitActions.connected();
+    } else if (response.status === 'not_authorized') {
+      window.location = '/';
+    } else {
+      FB.Event.subscribe('auth.authResponseChange', authenticate);
+      $('div.loading-spinner').hide();
+      $('div.fb-login-wrapper').show();
+    }
+  }
+};
+
+var getPreferences = function () {
+  var preferences = {};
+  $.ajax({
+    type: 'GET',
+    url: '/getPreferences.php',
+    dataType: 'json',
+    success: function (data) {
+      preferences = data.preferences || {};
+    },
+    async: false
+  });
+  return preferences;
+};
+
+var isRegistered = function () {
+  var registered;
+  $.ajax({
+    type: 'GET',
+    url: '/checkRegistered.php',
+    dataType: 'json',
+    success: function (data) {
+      registered = data.registered;
+    },
+    async: false
+  });
+
+  return registered;
+};
+
+// initalizing the fb lib
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: fbAppId,        // App ID
+    status: true,           // check login status
+    cookie: true,           // enable cookies to allow the server to access the session
+    xfbml: true            // parse page for xfbml or html5 social plugins like login button below
+  });
+  FB.getLoginStatus(authenticate);
+};
